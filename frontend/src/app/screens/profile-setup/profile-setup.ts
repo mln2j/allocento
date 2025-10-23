1103bb709db0e0ae7e5acf0e931f5da63fb0437c
mlendvaj <mihael.lendvaj@gmail.com>
2025-10-23 13:57:06 +0200

    Add core screens to frontend

---
+++ frontend/src/app/screens/profile-setup/profile-setup.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-setup',
+  imports: [],
+  templateUrl: './profile-setup.html',
+  styleUrl: './profile-setup.sass'
+})
+export class ProfileSetup {
+
+}
