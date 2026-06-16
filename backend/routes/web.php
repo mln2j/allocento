d7abf55e1078f9ea7e5c34503d80dcf97725dda1
mlendvaj <mihael.lendvaj@gmail.com>
2026-06-16 22:12:35 +0200

    feat: add lang query param support to email preview routes

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -6,11 +6,13 @@ Route::get('/', function () {
     return response()->json(['message' => 'API only'], 200);
 });
 
-// Email Previews
-Route::get('/mail-preview/verify', function () {
-    return new \App\Mail\VerifyEmailCodeMail('123456', 'Korisnik', 'hr');
+// Email Previews (use ?lang=en or ?lang=hr)
+Route::get('/mail-preview/verify', function (\Illuminate\Http\Request $request) {
+    $lang = $request->query('lang', 'hr');
+    return new \App\Mail\VerifyEmailCodeMail('123456', 'Korisnik', $lang);
 });
 
-Route::get('/mail-preview/reset', function () {
-    return new \App\Mail\ResetPasswordMail('654321', 'Korisnik', 'hr');
+Route::get('/mail-preview/reset', function (\Illuminate\Http\Request $request) {
+    $lang = $request->query('lang', 'hr');
+    return new \App\Mail\ResetPasswordMail('654321', 'Korisnik', $lang);
 });
