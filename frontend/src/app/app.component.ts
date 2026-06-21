0191b4b95581d66830a81bc06b58103e610e8346
mlendvaj <mihael.lendvaj@gmail.com>
2026-06-21 17:37:42 +0200

    feat: auto reload PWA on new version update

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToastComponent } from './core/layout/toast/toast.component';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { filter } from 'rxjs/operators';
+import { ToastService } from './core/services/toast.service';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +14,24 @@ import { ToastComponent } from './core/layout/toast/toast.component';
     <app-toast/>
   `,
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  private swUpdate = inject(SwUpdate);
+  private toastService = inject(ToastService);
+
+  ngOnInit() {
+    if (this.swUpdate.isEnabled) {
+      // Check for update immediately on load
+      this.swUpdate.checkForUpdate();
+
+      // Listen for when a new version is downloaded and ready
+      this.swUpdate.versionUpdates
+        .pipe(filter((evt): evt is VersionReadyEvent => evt.type === 'VERSION_READY'))
+        .subscribe(() => {
+          this.toastService.success('Nova verzija preuzeta, osvježavam...');
+          setTimeout(() => {
+            window.location.reload();
+          }, 1500);
+        });
+    }
+  }
+}
