d12e7a4d095403a2916bff0a282eb1e76b2d471c
mlendvaj <mihael.lendvaj@gmail.com>
2026-07-01 13:39:24 +0200

    Repo cleanup & .gitignore update

--- temp_script.php
+++
@@ -1,7 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
-$kernel->bootstrap();
-$ws = Illuminate\Support\Facades\DB::table('workspaces')->first();
-echo json_encode($ws, JSON_PRETTY_PRINT);
--- temp_script_user.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
-$kernel->bootstrap();
-
-$user = \App\Models\User::first();
-$user->load(['favoriteWorkspace', 'workspaces']);
-echo json_encode($user->toArray(), JSON_PRETTY_PRINT);
